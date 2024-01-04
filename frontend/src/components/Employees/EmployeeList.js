55abaa5a096c18a34602c3ea7df058de5cdfd245
Eric <ericmuthuipatch22@gmail.com>
2024-01-05 00:22:49 +0300

    created employee

---
+++ frontend/src/components/Employees/EmployeeList.js
@@ -0,0 +1,129 @@
+import React, { useEffect, useState } from 'react';
+import { Table, Button, ButtonToolbar } from 'react-bootstrap';
+import { FaEdit } from 'react-icons/fa';
+import { RiDeleteBin5Line } from 'react-icons/ri';
+import AddEmployeeModal from "./AddEmployeeModal"; // Import the new AddEmployeeModal
+import UpdateEmployeeModal from "./UpdateEmployeeModal"; // Import the new UpdateEmployeeModal
+import { getEmployees, deleteEmployee } from '../../services/EmployeesService'; // Import the new service
+import Pagination from '../Pagination';
+
+const EmployeesList = () => {
+  const [employees, setEmployees] = useState([]); 
+  const [addModalShow, setAddModalShow] = useState(false);
+  const [editModalShow, setEditModalShow] = useState(false);
+  const [editEmployee, setEditEmployee] = useState([]); 
+  const [isUpdated, setIsUpdated] = useState(false);
+  const [currentPage, setCurrentPage] = useState(0);
+  const itemsPerPage = 5;
+
+  useEffect(() => {
+    let mounted = true;
+    if (employees.length && !isUpdated) {
+      return;
+    }
+    getEmployees()
+      .then(data => {
+        if (mounted) {
+          setEmployees(data);
+        }
+      });
+    return () => {
+      mounted = false;
+      setIsUpdated(false);
+    };
+  }, [isUpdated, employees.length]);
+
+  const handleUpdate = (e, employee) => {
+    e.preventDefault();
+    setEditModalShow(true);
+    setEditEmployee(employee);
+  };
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    setAddModalShow(true);
+  };
+
+  const handleDelete = (e, id) => {
+    if (window.confirm('Are you sure?')) {
+      e.preventDefault();
+      deleteEmployee(id)
+        .then((result) => {
+          alert(result);
+          setIsUpdated(true);
+        })
+        .catch(() => {
+          alert("Failed to Delete Employee");
+        });
+    }
+  };
+
+  const handlePageChange = ({ selected }) => {
+    setCurrentPage(selected);
+  };
+
+  const offset = currentPage * itemsPerPage;
+
+  const handlePrint = () => {
+    window.print();
+  };
+
+  const pageCount = Math.ceil(employees.length / itemsPerPage);
+
+  return (
+    <div className="container-fluid side-container">
+      <div className="row side-row">
+        <header className='flex justify-between '>
+          <h2 className='text-2xl font-bold text-center pt-2'>Employees</h2>
+          <ButtonToolbar className='flex space-x-2'>
+            <Button className="" variant="primary" onClick={handleAdd}>
+              Add Employee
+            </Button>
+            <Button onClick={handlePrint} className="">
+              Print
+            </Button>
+            <AddEmployeeModal show={addModalShow} setUpdated={setIsUpdated} onHide={() => setAddModalShow(false)} />
+          </ButtonToolbar>
+        </header>
+
+        <div className="overflow-x-auto ">
+          <Table striped bordered hover className="react-bootstrap-table w-full " id="dataTable">
+            <thead className="sticky top-0 bg-gray-800 z-50 text-white">
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Job Title</th>
+                <th>Phone</th>
+                <th>Email</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {employees.slice(offset, offset + itemsPerPage).map((employee) => (
+                <tr key={employee.id}>
+                  <td>{employee.id}</td>
+                  <td>{employee.Name}</td>
+                  <td>{employee.Job_title}</td>
+                  <td>{employee.Phone}</td>
+                  <td>{employee.Email}</td>
+                  <td>
+                    <Button className="mr-2" onClick={(event) => handleUpdate(event, employee)}>
+                      <FaEdit />
+                    </Button>
+                    <Button className="mr-2" variant="danger" onClick={(event) => handleDelete(event, employee.id)}>
+                      <RiDeleteBin5Line />
+                    </Button>
+                    <UpdateEmployeeModal show={editModalShow} employee={editEmployee} setUpdated={setIsUpdated} onHide={() => setEditModalShow(false)} />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </div>
+        <Pagination pageCount={pageCount} handlePageChange={handlePageChange} />
+      </div>
+    </div>
+  );
+};
+
+export default EmployeesList;
---
+++ frontend/src/components/Visitors/VisitorList.js
@@ -0,0 +1,129 @@
+import React, { useEffect, useState } from 'react';
+import { Table, Button, ButtonToolbar } from 'react-bootstrap';
+import { FaEdit } from 'react-icons/fa';
+import { RiDeleteBin5Line } from 'react-icons/ri';
+import AddVisitorModal from "./AddVisitorModal";
+import UpdateVisitorModal from "./UpdateVisitorModal";
+import { getVisitors, deleteVisitor } from '../../services/VisitorsService';
+import Pagination from '../Pagination';
+
+const VisitorsList = () => {
+  const [visitors, setVisitors] = useState([]);
+  const [addModalShow, setAddModalShow] = useState(false);
+  const [editModalShow, setEditModalShow] = useState(false);
+  const [editVisitor, setEditVisitor] = useState([]);
+  const [isUpdated, setIsUpdated] = useState(false);
+  const [currentPage, setCurrentPage] = useState(0);
+  const itemsPerPage = 5;
+
+  useEffect(() => {
+    let mounted = true;
+    if (visitors.length && !isUpdated) {
+      return;
+    }
+    getVisitors()
+      .then(data => {
+        if (mounted) {
+          setVisitors(data);
+        }
+      });
+    return () => {
+      mounted = false;
+      setIsUpdated(false);
+    };
+  }, [isUpdated, visitors.length]);
+
+  const handleUpdate = (e, visitor) => {
+    e.preventDefault();
+    setEditModalShow(true);
+    setEditVisitor(visitor);
+  };
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    setAddModalShow(true);
+  };
+
+  const handleDelete = (e, id) => {
+    if (window.confirm('Are you sure?')) {
+      e.preventDefault();
+      deleteVisitor(id)
+        .then((result) => {
+          alert(result);
+          setIsUpdated(true);
+        })
+        .catch(() => {
+          alert("Failed to Delete Visitor");
+        });
+    }
+  };
+
+  const handlePageChange = ({ selected }) => {
+    setCurrentPage(selected);
+  };
+
+  const offset = currentPage * itemsPerPage;
+
+  const handlePrint = () => {
+    window.print();
+  };
+
+  const pageCount = Math.ceil(visitors.length / itemsPerPage);
+
+  return (
+    <div className="container-fluid side-container">
+      <div className="row side-row">
+        <header className='flex justify-between '>
+          <h2 className='text-2xl font-bold text-center pt-2'>Visitors</h2>
+          <ButtonToolbar className='flex space-x-2'>
+            <Button className="" variant="primary" onClick={handleAdd}>
+              Add Visitor
+            </Button>
+            <Button onClick={handlePrint} className="">
+              Print
+            </Button>
+            <AddVisitorModal show={addModalShow} setUpdated={setIsUpdated} onHide={() => setAddModalShow(false)} />
+          </ButtonToolbar>
+        </header>
+
+        <div className="overflow-x-auto ">
+          <Table striped bordered hover className="react-bootstrap-table w-full " id="dataTable">
+            <thead className="sticky top-0 bg-gray-800 z-50 text-white">
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Phone</th>
+                <th>Email</th>
+                <th>Red Flag</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {visitors.slice(offset, offset + itemsPerPage).map((visitor) => (
+                <tr key={visitor.id}>
+                  <td>{visitor.id}</td>
+                  <td>{visitor.Name}</td>
+                  <td>{visitor.Phone}</td>
+                  <td>{visitor.Email}</td>
+                  <td>{visitor.Red_flag ? 'Yes' : 'No'}</td>
+                  <td>
+                    <Button className="mr-2" onClick={(event) => handleUpdate(event, visitor)}>
+                      <FaEdit />
+                    </Button>
+                    <Button className="mr-2" variant="danger" onClick={(event) => handleDelete(event, visitor.id)}>
+                      <RiDeleteBin5Line />
+                    </Button>
+                    <UpdateVisitorModal show={editModalShow} visitor={editVisitor} setUpdated={setIsUpdated} onHide={() => setEditModalShow(false)} />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </div>
+        <Pagination pageCount={pageCount} handlePageChange={handlePageChange} />
+      </div>
+    </div>
+  );
+};
+
+export default VisitorsList;
